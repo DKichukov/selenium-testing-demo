43c3b571a573e318032371abd38e643e933fcaa1
DKichukov <dzhamal.kichukov@gmail.com>
2025-06-12 10:58:34 +0300

    use selenium locators

--- src/main/java/com/example/demo/LaunchChrome.java
+++ src/main/java/com/example/demo/LaunchChrome.java
@@ -1,16 +1,31 @@
 package com.example.demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
 
+  private static final String ABV_BG = "https://www.abv.bg/";
+
   public static void main(String[] args) {
     System.setProperty("webdriver.chrome.driver",
-        "C:\\JAVA\\projects\\Selenium_TestNG\\chrome-driver\\chromedriver-win64\\chromedriver.exe");
+        "chrome-driver/chromedriver-win64/chromedriver.exe");
+
     WebDriver driver = new ChromeDriver();
-    driver.get("https://amazon.in");
-    driver.close();
+    driver.get(ABV_BG);
+    driver.findElement(By.id("username")).sendKeys("abc");
+    driver.findElement(By.name("password")).sendKeys("1234");
+    driver.findElement(By.id("loginBut")).click();
+    String text = driver.findElement(By.id("form.errors")).getText();
+    System.out.println(text);
+
+//    driver.findElement(By.linkText("Забравена парола")).click();
+    driver.findElement(By.partialLinkText("парола")).click();
+    driver.navigate().back();
+    driver.navigate().forward();
+
+//    driver.quit();
   }
 
 }
