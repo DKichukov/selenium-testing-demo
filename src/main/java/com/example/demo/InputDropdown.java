91da4d4395598e4f6e8626f3fba3e5b53c0b8cef
DKichukov <dzhamal.kichukov@gmail.com>
2025-06-12 18:26:37 +0300

    use input and date picker

---
+++ src/main/java/com/example/demo/InputDropdown.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import static com.example.demo.constant.Urls.ESKY_BG;
+import static com.example.demo.util.WebDriverManager.createChromeDriver;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class InputDropdown {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    WebDriver driver = createChromeDriver();
+    driver.get(ESKY_BG);
+    driver.manage().window().maximize();
+
+    //oneway
+    Thread.sleep(1000);
+    driver.findElement(By.xpath("//input[@id='TripTypeOneway']")).click();
+
+    //From
+    Thread.sleep(1000);
+    driver.findElement(By.xpath("//input[@id='departureOneway']"))
+        .sendKeys("Варна, Варна, Варна, България (VAR)");
+
+    //To
+    Thread.sleep(1000);
+    driver.findElement(By.xpath("//input[@id='arrivalOneway']"))
+        .sendKeys("Виена, Швехат, Австрия (VIE)");
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ui-datepicker-calendar")));
+    //select date
+//    driver.findElement(By.xpath("//table[@class='ui-datepicker-calendar']//a[text()='12']")).click();
+    //select today()
+    driver.findElement(By.xpath("//table[@class='ui-datepicker-calendar']//td[contains(@class,'ui-datepicker-today')]//a")).click();
+    //search
+    driver.findElement(By.xpath("//button[contains(@class,'btn transaction qsf-search')]")).click();
+
+    Thread.sleep(10000);
+
+    driver.quit();
+  }
+
+}
