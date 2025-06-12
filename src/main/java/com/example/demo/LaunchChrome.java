c8004461a1afbc9499073ba25b3445673dae5f59
DKichukov <dzhamal.kichukov@gmail.com>
2025-06-12 09:44:36 +0300

    initial commit

---
+++ src/main/java/com/example/demo/LaunchChrome.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver",
+        "C:\\JAVA\\projects\\Selenium_TestNG\\chrome-driver\\chromedriver-win64\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://amazon.in");
+    driver.close();
+  }
+
+}
