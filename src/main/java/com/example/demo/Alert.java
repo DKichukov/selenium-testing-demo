4c94be7375ef0db547eaefbc6a5b829fe18edc97
DKichukov <dzhamal.kichukov@gmail.com>
2025-06-13 11:22:31 +0300

    add usage of alert

---
+++ src/main/java/com/example/demo/Alert.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import static com.example.demo.constant.Urls.W3SCHOOLS_COM;
+import static com.example.demo.util.WebDriverManager.createChromeDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Alert {
+
+  public static void main(String[] args) throws InterruptedException {
+    WebDriver driver = createChromeDriver();
+    driver.get(W3SCHOOLS_COM);
+
+//    driver.manage().window().maximize();
+
+    WebElement w = driver.findElement(By.xpath("//iframe[@id='iframeResult']"));
+
+    driver.switchTo().frame(w);
+    //click on try
+    driver.findElement(By.xpath("//button[@onclick='myFunction()']")).click();
+
+    //click on OK
+    Thread.sleep(3000);
+    driver.switchTo().alert().accept();
+
+    //Click on cancel
+//    driver.switchTo().alert().dismiss();
+
+    Thread.sleep(5000);
+    driver.quit();
+
+  }
+
+}
--- src/main/java/com/example/demo/constant/Urls.java
+++ src/main/java/com/example/demo/constant/Urls.java
@@ -4,6 +4,9 @@ public class Urls {
 
   public static final String ABV_BG = "https://www.abv.bg/";
   public static final String ESKY_BG = "https://www.esky.bg/";
+  public static final String W3SCHOOLS_COM = "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_confirm";
+  public static final String BOOK_MY_SHOW = "https://in.bookmyshow.com/explore/movies-pune";
+
 
   private Urls() {
   }
