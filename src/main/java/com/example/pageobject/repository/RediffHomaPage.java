85fc16ba534e8c773c083e2a5f9795080299ebaa
DKichukov <dzhamal.kichukov@gmail.com>
2025-06-16 18:15:17 +0300

    use page factory

--- src/main/java/com/example/pageobject/repository/RediffHomaPage.java
+++ src/main/java/com/example/pageobject/repository/RediffHomaPage.java
@@ -1,25 +1,35 @@
 package com.example.pageobject.repository;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RediffHomaPage {
 
   WebDriver driver;
-      By logo = By.xpath("(//div[@class='logo'])[1]");
-      By signIn = By.xpath("(//div[@class='logo'])[1]");
+//      By logo = By.xpath("(//div[@class='logo'])[1]");
+//      By signIn = By.xpath("(//div[@class='logo'])[1]");
+
+  @FindBy(xpath = "(//div[@class='logo'])[1]")
+  WebElement logo;
+
+  @FindBy(xpath = "(//div[@class='logo'])[1]")
+  WebElement signIn;
 
   public RediffHomaPage(WebDriver driver) {
     this.driver = driver;
+    PageFactory.initElements(driver,this);
   }
 
   public WebElement logo() {
-    return driver.findElement(logo);
+//    return driver.findElement(logo);
+    return logo;
   }
 
   public WebElement signIn() {
-    return driver.findElement(signIn);
+//    return driver.findElement(signIn);
+    return signIn;
   }
 
   public void username() {
